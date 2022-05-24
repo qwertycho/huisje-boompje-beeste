d5e7d9cbb3348ac0857a41537fbd3408ad4c5cd9
qwertycho <tychovopstal@gmail.com>
2022-05-24 11:23:07 +0200

    nav

---
+++ components/nav.js
@@ -0,0 +1,14 @@
+// import styles from '../styles/Footer.module.css'
+
+const nav = () => {
+    return (
+      <>
+        <header>
+        <img alt="logo"></img>
+        <h2>Villas4u</h2>
+        </header>
+      </>
+    );
+  };
+  
+  export default Footer;
