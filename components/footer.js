7f9102f58773eb9c95d20ad96677aa35b11963a4
qwertycho <tychovopstal@gmail.com>
2022-05-24 09:31:42 +0200

    footer component

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Footer.module.css'
+
+const Footer = () => {
+    return (
+      <>
+        <footer className={styles.footer}>
+        <p>Dit is de footer lol </p>
+        </footer>
+      </>
+    );
+  };
+  
+  export default Footer;
