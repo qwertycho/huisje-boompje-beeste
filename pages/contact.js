1b1c0f5364153994fd0f1e91006016a5cf232d21
RemcoHottentot <87954@glr.nl>
2022-05-24 09:44:35 +0200

    de paginas en css dingetjes

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Contact.module.css'
+import styles from '../styles/global.css'
+import footer from '../components/footer.js'
+
+export default function Home() {
+    return (
+
+    )
+}
