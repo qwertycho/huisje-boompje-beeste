dcc15aafd30bd0d7fa0f53a660c6e19051d32954
qwertycho <tychovopstal@gmail.com>
2022-05-30 14:05:33 +0200

    require

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import styles from '/styles/globals.css'
-import Footer from '/components/footer.js'
+const style = require('/styles/globals.css')
+const Footer = require('/components/footer.js')
 
 function MyApp({ Component, pageProps }) {
   return (
