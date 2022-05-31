16d0587e89a3dfb990ca3556ff94be5cd08ca612
qwertycho <tychovopstal@gmail.com>
2022-05-31 10:47:55 +0200

    init remco had nog niet moeten pullen

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// begin Remco afblijven
 // Requiring modules
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 var fs = require('fs');
-const port = 8000;
+const port = 80;
 
-// Render index.ejs file
+
+// dynamische rout gebaseerd op de param in de url
+app.get('/:id', function (req, res) {
+
+	ejs.renderFile('pages/' + req.params.id, {},
+		{}, function (err, template) {
+		if (err) {
+			throw err;
+		} else {
+			res.end(template);
+		}
+	});
+});
+
+
+// harde route naar pagina
 app.get('/', function (req, res) {
 
 	// Render page using renderFile method
